b33f8a26e8538ec4a3ba51de622469e1c87681b1
Ramixzx <ramiro.mijalovich@gmail.com>
2021-07-07 14:26:06 -0300

    Termine con el archivo de mediana.js. Converti el codigo inicial en una funciona, para que asi pueda ser utilizado pasandole cualquier tipo de arrays numerico. Faltaria hacer la union/interaccion_visual con el HTML

--- mediana.js
+++ mediana.js
@@ -1,43 +1,57 @@
 function calcularPromedioMedia (lista) {
+
     
     let totalLista = 0;
     
     for (let i = 0; i < lista.length; i++) {
         totalLista = totalLista + lista[i];
     }
-
+    
     let promedioFinal = totalLista / lista.length;
     
     return promedioFinal;
 }
-const listaP = [100, 300, 700, 800, 2000, 10000]
 
-let mitadListaP = parseInt(listaP.length / 2);
+function calcularMedia (lista) {
+    
+    lista.sort(
+        function (elementoA, elementoB) {
+            return elementoA - elementoB;
+        }
+    );
 
-function esPar(cantidad) {      
-    if (cantidad % 2 === 0) {
-        return true;
-    }else {
-        return false;
+    let mitadLista = parseInt(lista.length / 2);
+    
+    function esPar(cantidad) {      
+        if (cantidad % 2 === 0) {
+            return true;
+        }else {
+            return false;
+        }
     }
+    
+    let mediana; 
+    
+    if (esPar(lista.length)) {
+        const elemento1 = lista[mitadLista - 1];
+        const elemento2 = lista[mitadLista];
+    
+    const promedioElemento1y2 = calcularPromedioMedia([
+      elemento1,
+      elemento2,
+    ]);
+    
+        mediana = promedioElemento1y2;
+        console.log(mediana)
+    
+    } else {
+    
+    mediana = lista[mitadLista];
+    console.log(mediana);
+    }
+    
 }
 
-let mediana; 
+const listaP = [2000, 700, 800, 100, 300, 10000, 5000]
 
-if (esPar(listaP.length)) {
-    const elemento1 = listaP[mitadListaP - 1];
-    const elemento2 = listaP[mitadListaP];
 
-const promedioElemento1y2 = calcularPromedioMedia([
-  elemento1,
-  elemento2,
-]);
-
-    mediana = promedioElemento1y2;
-    console.log(mediana)
-
-} else {
-
-mediana = listaP[mitadListaP];
-console.log(mediana);
-}
